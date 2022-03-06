b0c693de83c21d80ff3ed68d48b75f65424ba393
Данил <@DESKTOP-U9PJUQE>
2022-03-06 13:43:28 +0700

    Добавьте файлы проекта.

---
+++ VP_lab3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VP_lab3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ VP_lab3/RomanNumberException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VP_lab3
+{
+    public class RomanNumberException : Exception
+    {
+        public RomanNumberException() : base("ERROR") { }
+        public RomanNumberException(string message) : base(message) { }
+    }
+}
